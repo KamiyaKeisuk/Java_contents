3e0a49df8ad046f0311067d8d352457230a3aa82
KamiyaKeisuk <91045221+KamiyaKeisuk@users.noreply.github.com>
2022-01-17 17:39:38 +0900

    Javaカリキュラム4章完了

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+    public static void main(String[] args) {
+        final int LUCKY_NUMBER = 7;
+        String str = "神谷　圭祐";
+        System.out.println(str);
+        str = "makima";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+    }
+}
